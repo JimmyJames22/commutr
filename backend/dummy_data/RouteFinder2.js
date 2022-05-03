fb510749d91dd3dbf1ad5448d4290826b383192e
James Millington <jameselliottmillington@gmail.com>
2022-05-03 02:09:58 -0400

    began work on RouteFinder2

--- backend/dummy_data/RouteFinder2.js
+++ backend/dummy_data/RouteFinder2.js
@@ -18,6 +18,9 @@ let students = [];
 
 let num_users;
 
+let i;
+let j;
+
 let route_dist_tolerance = 1.5; // maximum multiple of original commute time for drivers
 
 // weights need to be adjusted to help normalize
@@ -31,80 +34,77 @@ let routeList = [];
 let bestRoutesList = [];
 
 const logArr = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
+  for (i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 };
 
-class RouteFinder2 {
-  constructor() {}
+fillArrays();
+processDistances();
 
-  fillArrays() {
-    for (let i = 0; i < driver_data.length; i++) {
-      let user = new User(driver_data[i], route_dist_tolerance);
-      users.push(user);
-      drivers.push(user);
-    }
-
-    for (let i = 0; i < student_data.length; i++) {
-      let user = new User(student_data[i], route_dist_tolerance);
-      users.push(user);
-      students.push(user);
-    }
-
-    num_users = users.length;
-
-    console.log("Finished fillArrays()");
+function fillArrays() {
+  for (i = 0; i < driver_data.length; i++) {
+    let user = new User(driver_data[i], route_dist_tolerance);
+    users.push(user);
+    drivers.push(user);
   }
 
-  processDistances() {
-    for (let k = 0; k < users.length; k++) {
-      for (let l = k + 1; l < users.length; l++) {
-        userMap.push({
-          u1: users[k].uid,
-          u2: users[l].uid,
-          distance: users[k].distanceToUser(users[l]),
-        });
-      }
-    }
-
-    console.log("Finished processDistances()");
+  for (i = 0; i < student_data.length; i++) {
+    let user = new User(student_data[i], route_dist_tolerance);
+    users.push(user);
+    students.push(user);
   }
 
-  function calcEfficiency(total_dist, stops) {
-    let num_stops = stops.length;
+  num_users = users.length;
 
-    // calc time AADs
-    let arrival_ave = 0;
-    let departure_ave = 0;
-    for (let i = 0; i < num_stops; i++) {
-      arrival_ave += stops[i].arrival_time;
-      departure_ave += stops[i].departure_time;
-    }
-    arrival_ave /= num_stops;
-    departure_ave /= num_stops;
-    let arrival_aad = 0;
-    let departure_aad = 0;
-    for (let i = 0; i < num_stops; i++) {
-      arrival_aad += Math.abs(arrival_ave - stops[i].arrival_time);
-      derparture_aad += Math.abs(departure_ave - stops[i].departure_time);
-    }
-    arrival_aad /= num_stops;
-    departure_aad /= num_stops;
-
-    return (
-      num_stops * route_stops_weight +
-      total_dist * route_dist_weight -
-      Math.pow(arrival_aad, 2) * arrival_time_weight -
-      Math.pow(departure_aad, 2) * departure_time_weight
-    );
-  }
+  console.log("Finished fillArrays()");
 }
 
-const RF = new RouteFinder2();
-RF.fillArrays();
-RF.processDistances();
+function processDistances() {
+  for (let k = 0; k < users.length; k++) {
+    for (let l = k + 1; l < users.length; l++) {
+      userMap.push({
+        u1: users[k].uid,
+        u2: users[l].uid,
+        distance: users[k].distanceToUser(users[l]),
+      });
+    }
+  }
 
-console.log(drivers);
+  console.log("Finished processDistances()");
+}
 
-module.exports = RouteFinder2;
+function calcEfficiency(total_dist, stops) {
+  let num_stops = stops.length;
+
+  // calc time AADs
+  let arrival_ave = 0;
+  let departure_ave = 0;
+  for (let i = 0; i < num_stops; i++) {
+    arrival_ave += stops[i].arrival_time;
+    departure_ave += stops[i].departure_time;
+  }
+  arrival_ave /= num_stops;
+  departure_ave /= num_stops;
+  let arrival_aad = 0;
+  let departure_aad = 0;
+  for (let i = 0; i < num_stops; i++) {
+    arrival_aad += Math.abs(arrival_ave - stops[i].arrival_time);
+    derparture_aad += Math.abs(departure_ave - stops[i].departure_time);
+  }
+  arrival_aad /= num_stops;
+  departure_aad /= num_stops;
+
+  return (
+    num_stops * route_stops_weight +
+    total_dist * route_dist_weight -
+    Math.pow(arrival_aad, 2) * arrival_time_weight -
+    Math.pow(departure_aad, 2) * departure_time_weight
+  );
+}
+
+console.log(users);
+
+module.exports = {
+  calcEfficiency: calcEfficiency(),
+};
